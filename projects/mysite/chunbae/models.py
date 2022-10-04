9306187f23eaca84c5277f991da717476d4c7d41
oldjazz <dnpqshf@gmail.com>
2022-10-05 01:09:06 +0900

    view problem

--- projects/mysite/chunbae/models.py
+++ projects/mysite/chunbae/models.py
@@ -1,13 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
 
-class Question (models.Model) :
+class Question(models.Model):
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
 
-class Answer (models.Model) :
+
+class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
+
+
+# Test API 생성
+class Test(models.Model):
+    main = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.main
--- projects/mysite/chunbae/urls.py
+++ projects/mysite/chunbae/urls.py
@@ -1,21 +1,20 @@
 from django.contrib import admin
 from django.urls import path
+from rest_framework import routers
 from ninja import NinjaAPI
-
 from . import views
+from views import TestViewSet
 
 app_name = 'chunbae'
 
 api = NinjaAPI()
 
-
-@api.get("/add")
-def add(request, a: int, b: int):
-    return {"result": a + b}
+router = routers.DefaultRouter()
+router.register('test', TestViewSet)
 
 
 urlpatterns = [
     path('', views.index),
-    path("admin/", admin.site.urls),
-    path("api/", api.urls),
+    path('admin/', admin.site.urls),
+    path('test', TestViewSet.as_view())
 ]
