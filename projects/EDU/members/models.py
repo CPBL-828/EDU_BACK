ac5dacaed2cf4a5e7c1bfef8abed22817c1808ea
oldjazz <dnpqshf@gmail.com>
2022-11-15 20:00:50 +0900

    model edit #12

--- projects/EDU/members/models.py
+++ projects/EDU/members/models.py
@@ -55,7 +55,7 @@ class Student(models.Model):
     address = models.CharField(max_length=50, verbose_name='주소')
     remark = models.TextField(blank=True, verbose_name='특이사항')
     delState = models.CharField(max_length=1, verbose_name='삭제여부')
-    profileImg = models.CharField(max_length=50, blank=True, verbose_name='프로필사진링크')
+    profileImg = models.CharField(max_length=50, null=True, blank=True, verbose_name='프로필사진링크')
     createDate = models.DateTimeField(auto_now_add=True, verbose_name='생성일')
     editDate = models.DateTimeField(null=True, blank=True, verbose_name='수정일')
 
