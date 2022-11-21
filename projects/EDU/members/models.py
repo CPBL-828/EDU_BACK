83c6987ff8a7926d021a980899e9f8e47f79d07a
oldjazz <dnpqshf@gmail.com>
2022-11-21 20:01:06 +0900

    add model self reference

--- projects/EDU/members/models.py
+++ projects/EDU/members/models.py
@@ -12,15 +12,22 @@ class Teacher(models.Model):
     teacherKey = models.CharField(max_length=50, primary_key=True, default=uuid.uuid4, unique=True, editable=False,
                                   verbose_name='강사키')
     name = models.CharField(max_length=10, verbose_name='강사명')
-    id = models.CharField(max_length=50, unique=True, verbose_name='강사id')
+    id = models.CharField(max_length=50, null=True, blank=True, unique=True, verbose_name='강사id')
+
+    def save(self, *args, **kwargs):
+        self.id = self.name + '-' + str(shortuuid.ShortUUID(alphabet="0123456789").random(length=4))
+
+        super(Teacher, self).save(*args, **kwargs)
+
     phone = models.CharField(max_length=11, verbose_name='연락처')
-    email = models.CharField(max_length=50, verbose_name='이메일')
+    email = models.EmailField(max_length=50, verbose_name='이메일')
+    link = models.CharField(max_length=50, verbose_name='개인링크')
     part = models.CharField(max_length=10, verbose_name='담당')
     resSubject = models.CharField(max_length=10, verbose_name='담당과목')
     joinDate = models.DateField(null=True, verbose_name='입사일')
     leaveDate = models.DateField(null=True, verbose_name='퇴사일')
     resume = models.CharField(max_length=50, verbose_name='이력서링크')
-    profileImg = models.CharField(max_length=50, null= True, blank=True, verbose_name='프로필사진링크')
+    profileImg = models.CharField(max_length=50, null=True, blank=True, verbose_name='프로필사진링크')
     createDate = models.DateTimeField(auto_now_add=True, verbose_name='생성일')
     editDate = models.DateTimeField(null=True, blank=True, verbose_name='수정일')
 
@@ -30,7 +37,8 @@ class Teacher(models.Model):
 
 # 관리자 모델 생성
 class Admin(models.Model):
-    adminKey = models.CharField(max_length=50, primary_key=True, default=uuid.uuid4, unique=True, editable=False, verbose_name='관리자')
+    adminKey = models.CharField(max_length=50, primary_key=True, default=uuid.uuid4, unique=True, editable=False,
+                                verbose_name='관리자')
     type = models.CharField(max_length=10, verbose_name='관리자유형')
     id = ShortUUIDField(length=6, max_length=6, unique=True, editable=False, verbose_name='관리자id')
     createDate = models.DateTimeField(auto_now_add=True, verbose_name='생성일')
@@ -46,7 +54,13 @@ class Student(models.Model):
                                   verbose_name='학생키')
     parentKey = models.ForeignKey('Parent', on_delete=models.CASCADE, db_column='parentKey', verbose_name='부모키')
     name = models.CharField(max_length=50, verbose_name='학생명')
-    id = models.CharField(max_length=50, unique=True, verbose_name='학생id')
+    id = models.CharField(max_length=50, null=True, blank=True, unique=True, verbose_name='학생id')
+
+    def save(self, *args, **kwargs):
+        self.id = self.name + '-' + str(shortuuid.ShortUUID(alphabet="0123456789").random(length=4))
+
+        super(Student, self).save(*args, **kwargs)
+
     birth = models.DateField(verbose_name='생년월일')
     sex = models.CharField(max_length=1, verbose_name='성별')
     phone = models.CharField(max_length=11, verbose_name='연락처')
@@ -68,7 +82,13 @@ class Parent(models.Model):
     parentKey = models.CharField(max_length=50, primary_key=True, default=uuid.uuid4, unique=True, editable=False,
                                  verbose_name='부모키')
     name = models.CharField(max_length=10, verbose_name='이름')
-    id = models.CharField(max_length=50, unique=True, verbose_name='부모id')
+    id = models.CharField(max_length=50, blank=True, null=True, unique=True, verbose_name='부모id')
+
+    def save(self, *args, **kwargs):
+        self.id = self.name + '-' + str(shortuuid.ShortUUID(alphabet="0123456789").random(length=4))
+
+        super(Parent, self).save(*args, **kwargs)
+
     phone = models.CharField(max_length=11, verbose_name='연락처')
     createDate = models.DateTimeField(auto_now_add=True, verbose_name='생성일')
     editDate = models.DateTimeField(null=True, blank=True, verbose_name='수정일')
