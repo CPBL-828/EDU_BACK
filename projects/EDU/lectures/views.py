5afe1d3a28c9dc3d3daa34dcb656a71492c59f0f
oldjazz <dnpqshf@gmail.com>
2023-01-22 22:07:38 +0900

    ridiculous mistake :P #2

--- projects/EDU/lectures/views.py
+++ projects/EDU/lectures/views.py
@@ -486,12 +486,17 @@ def create_planner(request):
 
 @api_view(['POST'])
 def get_assign_list(request):
-    data = list(Assign.objects.filter(lectureKey=request.data['lectureKey']
-                                      ).values())
+    try:
+        if Assign.objects.filter(lectureKey=request.data['lectureKey']).exists():
+            data = list(Assign.objects.filter(lectureKey=request.data['lectureKey']
+                                              ).values())
 
-    result = {'resultData': data, 'count': len(data)}
+            result = {'resultData': data, 'count': len(data)}
 
-    return JsonResponse(result, status=200)
+            return JsonResponse(result, status=200)
+
+    except KeyError:
+        return JsonResponse({'chunbae': ' key 확인 : 요청에 필요한 키를 확인해주세요.'}, status=400)
 
 
 @api_view(['POST'])
@@ -569,7 +574,7 @@ def edit_assign(request):
             return JsonResponse({'chunbae': 'key 확인 바랍니다.'}, status=400)
 
     except KeyError:
-        return JsonResponse({'chunbae': '잘못된 요청입니다.'}, status=400)
+        return JsonResponse({'chunbae': ' key 확인 : 요청에 필요한 키를 확인해주세요.'}, status=400)
 
 
 @api_view(['POST'])
@@ -585,18 +590,21 @@ def delete_assign(request):
             return JsonResponse({'chunbae': '삭제되지 않았습니다.'}, status=400)
 
     except KeyError:
-        return JsonResponse({'chunbae': '잘못된 요청입니다.'}, status=400)
+        return JsonResponse({'chunbae': ' key 확인 : 요청에 필요한 키를 확인해주세요.'}, status=400)
 
 
 @api_view(['POST'])
 def get_test_list(request):
-    data = list(Test.objects.filter(lectureKey=request.data['lectureKey'], type__icontains=request.data['type'],
-                                    testDate__icontains=request.data['testDate']
-                                    ).values())
+    try:
+        if Test.objects.filter(lectureKey=request.data['lectureKey']).exists():
+            data = list(Test.objects.filter(lectureKey=request.data['lectureKey'], testType__icontains=request.data['type'],
+                                            testDate__icontains=request.data['testDate']
+                                            ).values())
 
-    result = {'resultData': data, 'count': len(data)}
+            result = {'resultData': data, 'count': len(data)}
 
-    return JsonResponse(result, status=200)
+    except KeyError:
+        return JsonResponse({'chunbae': ' key 확인 : 요청에 필요한 키를 확인해주세요.'}, status=400)
 
 
 @api_view(['POST'])
