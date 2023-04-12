8ea6af2e592e9b87a9dff08a37dd20d2a815aebf
OldtownJazz <dnpqshf@gmail.com>
2023-04-12 17:56:40 +0900

    fix file serve logic (experimental) #6

--- projects/EDU/config/urls.py
+++ projects/EDU/config/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from settings import base
+from config.settings import base
 
 urlpatterns = [
     path('admin/', admin.site.urls),
